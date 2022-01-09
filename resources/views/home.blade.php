b3ea836e0a11b84553e16eeeec1e9b0781bf2960
Paul Kristopher Espina <kristopherespina@gmail.com>
2022-01-09 16:04:42 +0800

    [ADD] Home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,20 +3,43 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+        <div class="col-8 text-white">
 
+            <div class="card border-secondary bg-transparent mx-5 text-white">
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    <label for="jokecontent"><h3>Got something funny?</h5></label>
+                    <textarea id="jokecontent" class="form-control bg-transparent text-white border-secondary mb-3" rows="5"></textarea>
+                    <input class="btn btn-primary" type="submit" value="Joke">
                 </div>
             </div>
+            
+            <hr class="mt-5">
+
+            <div class="card border-secondary bg-transparent mx-5 text-white">
+                <div class="card-body">
+                    <div class="card-title fw-bold">
+                        <img src="https://cdn.pixabay.com/photo/2016/11/08/15/21/user-1808597_960_720.png" class="img-circle width-32" width="32px" height="32px">
+                        <a href="#" class="text-decoration-none">@paul</a>
+                    </div>
+                    <div class="card-text">
+                        If you donate one kidney, everybody loves you and you're a total hero. But donate five and suddenly everyone is yelling.
+                    </div>
+                </div>
+                <div class="card-footer text-secondary border-secondary">
+                    <div class="row ">
+                        <div class="col">
+                            <button class="btn" type="submit">
+                                <img src="{{ URL::to('/') }}/testimage/upvote.png" width="16px" height="16px">
+                            </button>
+                            0
+                        </div>
+                        <div class="col text-end">
+                            Dec 2 2021 7:23 AM
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 </div>
